d64f27a69765324fc6ef93354f9c9cda994ed8de
dan1l-git <danilgavriluk756@gmail.com>
2025-05-14 21:25:55 +0300

    Add AppDatabase singleton configuration.

---
+++ app/src/main/java/com/example/autoparkapp/AppDatabase.java
@@ -0,0 +1,29 @@
+package com.example.autoparkapp;
+
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+@Database(entities = {CarEntity.class}, version = 1)
+public abstract class AppDatabase extends RoomDatabase {
+
+    private static volatile AppDatabase INSTANCE;
+
+    public abstract CarDao carDao();
+
+    public static AppDatabase getInstance(Context context) {
+        if (INSTANCE == null) {
+            synchronized (AppDatabase.class) {
+                if (INSTANCE == null) {
+                    INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
+                                    AppDatabase.class, "autopark.db")
+                            .fallbackToDestructiveMigration()
+                            .build();
+                }
+            }
+        }
+        return INSTANCE;
+    }
+}
